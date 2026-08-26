22ab04dca4d068e8c1173fe24330084052517f7b
Najaf-14 <najafimran314@gmail.com>
2026-08-26 17:36:02 +0500

    added routes

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const authRoutes = require("./src/routes/authRoutes");
+
 const { initDB } = require("./src/config/db");
+
+// routes
+app.use("/api/auth", authRoutes);
 
 //Test API
 app.get("/", (req, res) => {
