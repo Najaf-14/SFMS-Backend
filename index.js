624412a959fab2603004b73c2388ff1b27b7d067
Najaf-14 <najafimran314@gmail.com>
2026-08-26 22:42:28 +0500

    db connection pool changes and family addition

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// routes paths
 const authRoutes = require("./src/routes/authRoutes");
+const familyRoutes = require("./src/routes/familyRoutes");
 
 const { initDB } = require("./src/config/db");
 
-// routes
+// api routes
 app.use("/api/auth", authRoutes);
+app.use("/api/families", familyRoutes);
 
 //Test API
 app.get("/", (req, res) => {
@@ -21,10 +24,14 @@ app.get("/", (req, res) => {
   });
 });
 
-initDB();
+const startServer = async () => {
+  await initDB();
 
-const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on PORT:${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server running on PORT:${PORT}`);
+  });
+};
+
+startServer();
