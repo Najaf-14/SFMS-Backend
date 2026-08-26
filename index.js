7f5abf3a8c8ff96350e056dd2776bf5fe44d20c9
Najaf-14 <najafimran314@gmail.com>
2026-08-26 15:31:53 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//Test API
+app.get("/", (req, res) => {
+  res.json({
+    message: "School API is running ",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT:${PORT}`);
+});
