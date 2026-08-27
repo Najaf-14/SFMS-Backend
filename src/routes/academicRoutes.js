49257c96d0f3e85a2db74da7efc2aa2546b4272b
Najaf-14 <najafimran314@gmail.com>
2026-08-27 19:55:13 +0500

    sessions

--- src/routes/academicRoutes.js
+++ src/routes/academicRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
+
 const {
   addClass,
   fetchClasses,
   fetchClassById,
   addSection,
   fetchSectionsByClass,
+  addAcademicSession,
+  fetchAcademicSessions,
+  fetchAcademicSessionById,
+  editAcademicSession,
 } = require("../controllers/academicController");
 const { authenticateToken } = require("../middleware/authMiddleware");
 const { authorizeRoles } = require("../middleware/roleMiddleware");
@@ -25,5 +30,19 @@ router.post(
   addSection,
 );
 router.get("/classes/:classId/sections", fetchSectionsByClass);
+
+router.post(
+  "/sessions",
+  authorizeRoles("SUPER_ADMIN", "ADMIN", "PRINCIPAL"),
+  addAcademicSession,
+);
+
+router.get("/sessions", fetchAcademicSessions);
+router.get("/sessions/:id", fetchAcademicSessionById);
+router.patch(
+  "/sessions/:id",
+  authorizeRoles("SUPER_ADMIN", "ADMIN", "PRINCIPAL"),
+  editAcademicSession,
+);
 
 module.exports = router;
