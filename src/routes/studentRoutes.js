a80297aadef51181d71d8dce885e9b653ba1a88f
Najaf-14 <najafimran314@gmail.com>
2026-08-27 12:26:32 +0500

    student

---
+++ src/routes/studentRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const {
+  addStudent,
+  fetchAllStudents,
+  fetchStudentById,
+} = require("../controllers/studentController");
+const { authenticateToken } = require("../middleware/authMiddleware");
+const { authorizeRoles } = require("../middleware/roleMiddleware");
+
+router.use(authenticateToken);
+
+router.post(
+  "/",
+  authorizeRoles("SUPER_ADMIN", "ADMIN", "PRINCIPAL"),
+  addStudent,
+);
+router.get("/", fetchAllStudents);
+router.get("/:id", fetchStudentById);
+
+module.exports = router;
